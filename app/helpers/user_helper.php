ce8c4d1dd8273f8b4ea5b5cd5642d660bc30b5c1
harontaiko <harontaiko@gmail.com>
2021-07-01 17:40:18 +0300

    enable nd_mysql and disable mysqli

--- app/helpers/user_helper.php
+++ app/helpers/user_helper.php
@@ -12,3 +12,15 @@ $stmt->execute();
 
 return $stmt;
 }
+
+//verify mail and token
+function verifyTokenAndmail($table, $email, $db)
+{
+  $query = "SELECT * FROM " . $table . " WHERE email=?";
+
+  $stmt = $db->prepare($query);
+  $stmt->bind_param("s", $email);
+  $stmt->execute();
+
+  return $stmt;
+}
